fa4c8e93657d3d417013cf02d2fbc561b1498f98
Alexandre Vieira <calexandrelvieira@gmail.com>
2020-06-23 10:55:50 -0300

    Página de blog

--- index.php
+++ index.php
@@ -1,64 +1,74 @@
 <?php get_header(); ?>
 
 <div class="container">
-    <div class="row">
-        <div class="section col l9">
-            <h1 class="header-text"><?php echo single_cat_title(); ?></h1>
+    <section class="section columns is-variable is-6 is-multiline">
+        <div class="column is-full">
+            <h1 class="projects-title"><?php single_post_title(); ?></h1>
+        </div>
+        <div class="column is-four-fifths">
+            <div class="blog">
+            <?php
+            global $wp_query;
+            
+            $big = 999999999;
+            $links = paginate_links( array(
+                'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+                'format' => '?paged=%#%',
+                'current' => max( 1, get_query_var('paged') ),
+                'total' => $wp_query->max_num_pages,
+                'prev_text' => '<i class="fas fa-md fa-chevron-left"></i>',
+                'next_text' => '<i class="fas fa-md fa-chevron-right"></i>',
+                'type' => 'array',
+            ) );
 
-            <div class="row">
-                <?php get_template_part( 'includes/section', 'archive' ); ?>
-            </div>
+            if ( $links ):
+                $current = max( 1, get_query_var( 'paged' ) );
+                $total_pages = count($links);
 
-            <div class="row">
-                <?php
-                    global $wp_query;
-                    
-                    $big = 999999999;
-                    $links = paginate_links( array(
-                        'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-                        'format' => '?paged=%#%',
-                        'current' => max( 1, get_query_var('paged') ),
-                        'total' => $wp_query->max_num_pages,
-                        'prev_text' => '<i class="material-icons">chevron_left</i>',
-                        'next_text' => '<i class="material-icons">chevron_right</i>',
-                        'type' => 'array',
-                    ) );
+                $available_links = array_map( function($val, $key) use ($current, $total_pages) {
+                    $page = $current > 1 && $current < $total_pages ? $key : $key + 1;
 
-                    if ( $links ):
-                        $current = max( 1, get_query_var( 'paged' ) );
-                        $total_pages = count($links);
+                    return $page !== $current
+                        ? '<li class="waves-effect">'. $val . '</li>'
+                        : '<li class="active"><a href="#!">'. $current . '</a></li>';
+                }, $links, array_keys($links) );
 
-                        $available_links = array_map( function($val, $key) use ($current, $total_pages) {
-                            $page = $current > 1 && $current < $total_pages ? $key : $key + 1;
+                echo '<ul class="pagination">';
+                if ( $prev_posts_link = get_previous_posts_link() && 0 == 1):
+                    echo '<li class="waves-effect">'. $prev_posts_link .'</li>';
+                endif;
 
-                            return $page !== $current
-                                ? '<li class="waves-effect">'. $val . '</li>'
-                                : '<li class="active"><a href="#!">'. $current . '</a></li>';
-                        }, $links, array_keys($links) );
+                echo join( '', $available_links);
 
-                        echo '<ul class="pagination">';
-                        if ( $prev_posts_link = get_previous_posts_link() && 0 == 1):
-                            echo '<li class="waves-effect">'. $prev_posts_link .'</li>';
-                        endif;
+                if ( $next_posts_link = get_next_posts_link() && 0 == 1):
+                    echo '<li class="waves-effect">'. $next_posts_link .'</li>';
+                endif;
 
-                        echo join( '', $available_links);
-
-                        if ( $next_posts_link = get_next_posts_link() && 0 == 1):
-                            echo '<li class="waves-effect">'. $next_posts_link .'</li>';
-                        endif;
-
-                        echo '</ul>';
-                    endif;
-                ?>
+                echo '</ul>';
+            
+            else: while ( $wp_query->have_posts() ): $wp_query->the_post(); ?>
+                <div class="blog-card">
+                    <a href="<?php the_permalink(); ?>">
+                        <img class="blog-card-image" src="<?php the_post_thumbnail_url( 'blog-large' ); ?>" alt="<?php the_title(); ?>" />
+                        
+                    </a>
+                    <div class="blog-card-content">
+                        <h4 class="blog-card-title"><?php the_title(); ?></h4>
+                        <?php the_excerpt(); ?>
+                        <a class="button is-dark" href="<?php the_permalink(); ?>">Ler mais</a>
+                    </div>
+                </div>
+            <?php endwhile; endif; ?>
             </div>
         </div>
-        <div class="section col l3">
-            <?php if( is_active_sidebar( 'blog-sidebar' ) ):?>
-                <?php get_search_form(); ?>
-                <?php dynamic_sidebar( 'blog-sidebar' ); ?>
-            <?php endif;?>
+        <div class="column is-one-fifth">
+        <?php 
+            if( is_active_sidebar( 'blog-sidebar' ) ):
+                dynamic_sidebar( 'blog-sidebar' );
+            endif;
+        ?>
         </div>
-    </div>
+    </section>
 </div>
 
 <?php get_footer(); ?>
