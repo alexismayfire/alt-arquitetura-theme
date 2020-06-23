de587b5b0e64b4abd177832a43068e80b1cd8cbc
Alexandre Vieira <calexandrelvieira@gmail.com>
2020-06-23 10:56:26 -0300

    Página individual de projeto

--- single-project.php
+++ single-project.php
@@ -6,9 +6,22 @@
             <h1 class="projects-title"><?php the_title(); ?></h1>
         </div>
         <div class="column is-full">
-        <?php foreach ( get_attached_media( 'image', $post ) as $img): ?>
-            <img src="<?php echo wp_get_attachment_image_url( $img->ID, 'project-gallery' ); ?>"/>
-        <?php endforeach; ?>
+            <ul class="project-detail">
+                <li>Cliente <strong><?php the_field( 'projeto_cliente');?></strong></li>
+                <li>Segmento <strong><?php the_field( 'projeto_segmento');?></strong></li>
+                <li>Área Construída <strong><?php echo get_field( 'projeto_area') ? get_field( 'projeto_area' ).' m<sup>2</sup>' : '-' ; ?></strong></li>
+                <li>Status <strong><?php the_field( 'projeto_status');?></strong></li>
+            </ul>
+        </div>
+        <div class="column is-full">   
+        <?php 
+        if ( have_posts() ): 
+            while ( have_posts() ): 
+                the_post();
+                the_content();
+            endwhile;
+        endif;
+        ?>
         </div>
     </section>
 </div>
