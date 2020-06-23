86ad6b01ab081d2d169f55d9089f17f3feab61ae
Alexandre Vieira <calexandrelvieira@gmail.com>
2020-06-23 14:19:01 -0300

    Correções na página de projeto individual

--- single-project.php
+++ single-project.php
@@ -1,16 +1,16 @@
 <?php get_header(); ?>
 
 <div class="container">
-    <section class="section columns-is-variable is-6 is-multiline">
+    <section class="section columns-is-variable is-6 is-multiline pb-0">
         <div class="column is-full">
-            <h1 class="projects-title"><?php the_title(); ?></h1>
+            <h1 class="project-title"><?php the_title(); ?></h1>
         </div>
         <div class="column is-full">
             <ul class="project-detail">
-                <li>Cliente <strong><?php the_field( 'projeto_cliente');?></strong></li>
-                <li>Segmento <strong><?php the_field( 'projeto_segmento');?></strong></li>
-                <li>Área Construída <strong><?php echo get_field( 'projeto_area') ? get_field( 'projeto_area' ).' m<sup>2</sup>' : '-' ; ?></strong></li>
-                <li>Status <strong><?php the_field( 'projeto_status');?></strong></li>
+                <li><span>Cliente</span><strong><?php the_field( 'projeto_cliente');?></strong></li>
+                <li><span>Segmento</span><strong><?php $cat = get_the_terms( $post, 'segments' ); echo $cat[0]->name; ?></strong></li>
+                <li><span>Área Construída</span><strong><?php echo get_field( 'projeto_area') ? get_field( 'projeto_area' ).' m<sup>2</sup>' : '-' ; ?></strong></li>
+                <li><span>Status</span><strong><?php the_field( 'projeto_status');?></strong></li>
             </ul>
         </div>
         <div class="column is-full">   
@@ -22,6 +22,18 @@
             endwhile;
         endif;
         ?>
+        </div>
+    </section>
+    <section class="section columns is-variable is-6 is-multiline pt-0" id="contato">
+        <div class="column is-full">
+            <h2 class="section-title"><span class="section-title-prepend">entre em </span>contato</h2>
+        </div>
+        <div class="column is-full">
+            <p>Queremos conversar sobre os seus projetos.</p>
+            <p>Use essa área para tirar dúvidas, enviar questões e comentários.</p>
+        </div>
+        <div class="column is-full form-wrapper">
+            <?php echo do_shortcode( '[contact-form-7 id="98" title="Contato Home"]' ); ?>
         </div>
     </section>
 </div>
