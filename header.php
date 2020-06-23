e54c789c5f477731ba38fe12c382da5441d64db7
Alexandre Vieira <calexandrelvieira@gmail.com>
2020-06-23 10:53:38 -0300

    Alteração do título das páginas

--- header.php
+++ header.php
@@ -3,7 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
-    <title><?php the_title() ?></title>
+    <title><?php 
+        $name = get_bloginfo( 'name' );
+        $after = is_front_page() ? get_bloginfo( 'description' ) : wp_title( '|', false, 'left' );
+    
+        echo $name . $after;
+    ?></title>
     
     <?php wp_head(); ?>
 </head>
