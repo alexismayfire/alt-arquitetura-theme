21679890000200f4c24753ed627011a85338b90c
Alexandre Vieira <calexandrelvieira@gmail.com>
2020-06-23 10:57:16 -0300

    Alterações de estilização do projeto Busca no blog Geração do CSS

--- searchform.php
+++ searchform.php
@@ -1,5 +1,6 @@
 <form action="/" method="get">
-    <label for="search">Pesquise em nossos posts</label>
-    <input type="text" name="s" id="search" value="<?php the_search_query(); ?>" required /> 
-    <button type="submit">Buscar</button>
+    <div class="form-control">
+        <input class="input" type="text" name="s" id="search" value="<?php the_search_query(); ?>" required /> 
+        <button class="button is-dark" type="submit"><i class="fas fa-search"></i></button>
+    </div>
 </form>
