bac13676a67eed48d13d61d849b07daa5523d290
Alexandre Vieira <calexandrelvieira@gmail.com>
2020-06-29 02:46:46 -0300

    Suporte a title-tag no tema Nova tag para manipulação dos títulos das páginas

--- functions.php
+++ functions.php
@@ -10,6 +10,7 @@ require_once get_template_directory() . '/core/scripts.php';
 add_theme_support( 'menus' );
 add_theme_support( 'post-thumbnails' );
 add_theme_support( 'widgets' );
+add_theme_support( 'title-tag' );
 add_theme_support(
     'html5',
     array(
--- header.php
+++ header.php
@@ -3,12 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
-    <title><?php 
-        $name = get_bloginfo( 'name' );
-        $after = is_front_page() ? get_bloginfo( 'description' ) : wp_title( '|', false, 'left' );
-    
-        echo $name . $after;
-    ?></title>
     
     <?php wp_head(); ?>
 </head>
