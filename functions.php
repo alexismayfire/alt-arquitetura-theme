9b4e0f3848d92dfeac9a62e3cb3d3ae7a56888c9
Alexandre Vieira <calexandrelvieira@gmail.com>
2020-08-16 18:28:28 -0300

    Release da versão 1.1.0

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-define( 'THEME_VERSION', '1.1.0');
+define( THEME_VERSION, '1.1.0');
 
 require_once get_template_directory() . '/core/post-types.php';
 require_once get_template_directory() . '/core/rest-api.php';
